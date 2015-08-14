5bfd538a54742d06275adff317c4dca24d6080ae
tarun <cooltarunsharma111@gmail.com>
2015-08-14 10:51:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Material Design
+
+ Material Design which was introduced in Android Lollipop version. In Material Design lot of new things were introduced like Material Theme, new widgets, custom shadows, vector drawables and custom animations. If you haven’t working on Material Design yet, this article will give you a good start.
+
+# Android Studio App 
+
+> Material Design Specifications[Design Specifications](http://www.google.com/design/spec/material-design/introduction.html#) 
+> Creating Apps with Material Design[Material Design](http://developer.android.com/training/material/index.html) 
+
+## Running Locally
+Make sure you have [Android studio/Eclipse ADB](http://developer.android.com/tools/studio/index.html) 
+
+```sh
+clone your own fork or download the copy 
+import project in app studio
+run gradle build 
+```
+
+## Material Design Color Customization
+Material Design provides set of properties to customize the Material Design Color theme. But we use five primary attributes to customize overall theme.
+
+### colorPrimaryDark – This is darkest primary color of the app mainly applies to notification bar background.
+
+### colorPrimary – This is the primary color of the app. This color will be applied as toolbar background.
+
+### textColorPrimary – This is the primary color of text. This applies to toolbar title.
+
+### windowBackground – This is the default background color of the app.
+
+### navigationBarColor – This color defines the background color of footer navigation bar.
+
+
---
+++ app/src/main/java/gennexttraining/tarunsoft/com/gennextweb/MainActivity.java
@@ -0,0 +1,62 @@
+package gennexttraining.tarunsoft.com.gennextweb;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import gennexttraining.tarunsoft.com.gennextweb.util.FragmentDrawer;
+
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
+
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+
+    }
+}
